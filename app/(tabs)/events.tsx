18bd1316125126c0b368927100eaa17dce346239
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2025-11-17 15:54:54 -0500

    update tab layout (home, event, profile) and remove unused screens

---
+++ app/(tabs)/events.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+export default function EventsScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Events</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
