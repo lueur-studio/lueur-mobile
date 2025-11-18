bb5f0aec8f3ba62bda31c62faa4d6ea0c1e26c3c
BarbodH <habibi.k.barbod@gmail.com>
2025-11-18 05:54:17 -0500

    Migrate to Nativewind styling + remove StyleSheet objects

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,19 +1,10 @@
-import { StyleSheet } from "react-native";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView className="flex-1 justify-center items-center">
       <ThemedText type="title">Home</ThemedText>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { Button } from "@react-navigation/elements";
@@ -6,17 +5,9 @@ import { router } from "expo-router";
 
 export default function LoginScreen() {
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView className="flex-1 justify-center items-center">
       <ThemedText type="title">Login</ThemedText>
       <Button onPress={() => router.replace("/(tabs)/home")}>Sign In</Button>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
