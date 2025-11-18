2da9598744f5347f77c04c7e702a3123738b680e
BarbodH <habibi.k.barbod@gmail.com>
2025-11-18 05:47:57 -0500

    Set up Nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import "./global.css";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
